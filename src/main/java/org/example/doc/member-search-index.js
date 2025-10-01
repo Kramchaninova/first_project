e74b4c171cb8f6d7603dd42fc538d8a4dd3e8898
nika <vojtovanika07@gmail.com>
2025-10-01 16:58:58 +0500

    doc

--- src/main/java/org/example/doc/member-search-index.js
+++
@@ -1,1 +0,0 @@
-memberSearchIndex = [{"p":"org.example","c":"Main","l":"Main()","u":"%3Cinit%3E()"},{"p":"org.example","c":"Main","l":"main(String[])","u":"main(java.lang.String[])"},{"p":"org.example","c":"Main","l":"startBot(String)","u":"startBot(java.lang.String)"}];updateSearchResults();
